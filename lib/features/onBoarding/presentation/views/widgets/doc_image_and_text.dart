c39a541bc9bfa11eee9253a926f57f527d284291
MarioAshraf <mariioashraf1@gmail.com>
2024-09-02 04:58:37 +0300

    added onBoarding feature

---
+++ lib/features/onBoarding/presentation/views/widgets/doc_image_and_text.dart
@@ -0,0 +1,46 @@
+import 'package:doctor_appointment_app/core/theming/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+class DocImageAndText extends StatelessWidget {
+  const DocImageAndText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          bottom: 50,
+          top: 0,
+          right: 30,
+          left: 0,
+          child: SvgPicture.asset(
+            'assets/svgs/logo_low_opacity.svg',
+          ),
+        ),
+        Container(
+          foregroundDecoration: BoxDecoration(
+            gradient: LinearGradient(
+              end: Alignment.topCenter,
+              begin: Alignment.bottomCenter,
+              colors: [Colors.white, Colors.white.withOpacity(0)],
+              stops: const [0.14, 0.4],
+            ),
+          ),
+          child: Image.asset(
+            'assets/images/onBoarding_doctor.png',
+          ),
+        ),
+        Positioned(
+          bottom: 20,
+          right: 0,
+          left: 0,
+          child: Text(
+            'Best Doctor\nAppointment App',
+            textAlign: TextAlign.center,
+            style: TextStyles.font23BlueBold,
+          ),
+        )
+      ],
+    );
+  }
+}
