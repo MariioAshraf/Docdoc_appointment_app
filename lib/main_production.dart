b265a5c07f85952f7672fc1f829a6f8eb097111c
MarioAshraf <mariioashraf1@gmail.com>
2024-09-14 21:48:21 +0300

    - secured firebase_cli_token with github secrets - added firebase again to handle errors because google-service.json do not find .dev for flavor development - added fastlane with it's configurations - added android/app/src/development/google-services.json to handle these errors - added workflow for android release with fastlane and firebase app distribution - bumped version to 1.0.3

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -5,7 +5,7 @@ import 'core/routing/app_router.dart';
 import 'doc_app.dart';
 
 void main()async {
-await ScreenUtil.ensureScreenSize();
+  await ScreenUtil.ensureScreenSize();
   serviceLocator();
   runApp(DocApp(
     appRouter: AppRouter(),
